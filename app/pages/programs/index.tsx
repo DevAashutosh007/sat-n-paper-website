03d91b134e94be967c21fd41384f4e2ce3e70cfe
TheLeftyCoder <aashutoshmishra296@gmail.com>
2025-06-03 12:12:09 +0530

    minor fix in import

--- app/pages/programs/index.tsx
+++ app/pages/programs/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { coursePrograms } from "@/data/coursePrograms";
+import { coursePrograms } from "@/app/data/coursePrograms";
 
 const CourseProgramsPage = () => {
   return (
